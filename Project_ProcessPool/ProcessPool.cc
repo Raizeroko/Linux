a369c26ce97eda884f06c9a4ea53ca68ee7e8e00
Raizeroko <1464001349@qq.com>
2024-12-05 19:16:13 +0800

    2024_12_05 Project_ProcessPool

--- Project_ProcessPool/ProcessPool.cc
+++ Project_ProcessPool/ProcessPool.cc
@@ -1,7 +1,7 @@
 #include "Task.hpp"
 
-const int processnum = 5;
 
+const int tasknum = 5;
 // 输入型参数：const &
 // 输出型参数：*
 // 输入输出型参数：&
@@ -15,14 +15,30 @@ public:
         ,_processname(processname)
     {}
 
-private:
     int _fd;
     pid_t _id;
     string _processname; 
 };
 
+void excuteTask()
+{
+    int which;
+    while(true){
+        // process will be blocked
+        int readBytes = read(0, &which, sizeof(which));
+        if(readBytes == 0) break;
+        cout << "readBytes: " << readBytes << endl;
+        Task t;
+        printf("child: %d ->", getpid());
+        t[which];
+    }
+    
+}
+
+
 void initProcessPool(vector<channel>& channels){
-    for(int i = 0; i < processnum; i++){
+    vector<int> oldfd;
+    for(int i = 0; i < Task()._tasknum; i++){
         int pipefd[2];
         pipe(pipefd);
         pid_t id = fork();
@@ -32,13 +48,17 @@ void initProcessPool(vector<channel>& channels){
         }
         if(id == 0){
             // child
+            // close father write pipe
+            for(auto &e: oldfd)
+            {
+                close(e);
+            }
+
             // close pipe write
             close(pipefd[1]);
             // redirection to no.0 filefd, this will close stdin
-            dup2(pipefd[0], 0);
-            char buf[1024];
-            // process will be blocked
-            read(pipefd[0], buf, sizeof(buf));
+            dup2(pipefd[0], 0);            
+            excuteTask();
             exit(0);
         }
         // father
@@ -47,12 +67,51 @@ void initProcessPool(vector<channel>& channels){
         string processname = "process - " + to_string(i);
         channel c(pipefd[1], id, processname);
         channels.push_back(c);
+        oldfd.push_back(pipefd[1]);
     }
 }
 
+void menu()
+{
+    cout << "*********************************" << endl;
+    cout << "* 1.野怪刷新         2.小兵刷新 *" << endl;
+    cout << "* 3.释放技能         4.对局开始 *" << endl;
+    cout << "* 5.对局结束         6.退出游戏 *" << endl;
+    cout << "*********************************" << endl;
 
-void controlProcess(vector<channel> channels){
+}
 
+
+void controlProcess(vector<channel>& channels){
+    menu();
+    int task = 0;
+    while(true){
+        int c = rand() % channels.size();
+        cout << "选择任务:";
+        cin >> task;
+        if(task == 6)
+        {
+            break;  
+        } 
+        printf("father send: %d to channel[%d]\n", task, c);
+        write(channels[c]._fd, &task, sizeof(task));
+        sleep(1);
+    }
+    cout << "finish control"<<endl;
+
+}
+
+void quitProcess(vector<channel>& channels){
+    for(int i = 0; i < channels.size(); i++)
+    {
+        close(channels[i]._fd);
+        cout << "finish close: "<< channels[i]._fd << endl;
+        waitpid(channels[i]._id, nullptr, 0);
+        cout << "finish wait: "<< channels[i]._id << endl;   
+    
+    }
+
+    cout << "Finish quit" <<endl;
 }
 
 
@@ -67,6 +126,7 @@ int main()
     // 控制子进程
     controlProcess(channels);
     // 清理收尾
+    quitProcess(channels);
     return 0;
 
 }
--- Project_ProcessPool/Task.hpp
+++ Project_ProcessPool/Task.hpp
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <iostream>
 #include <stdlib.h>
@@ -8,3 +9,50 @@
 #include <sys/wait.h>
 
 using namespace std;
+
+
+class Task{
+public:
+    Task()
+        :_tasknum(5)
+    {}
+
+    void task1(){
+        cout<<"野怪刷新"<<endl;
+    }
+
+    void task2(){
+        cout<<"小兵刷新"<<endl;
+
+    } 
+
+    void task3(){
+        cout<<"释放技能"<<endl;
+
+    }
+
+    void task4(){
+        cout<<"对局开始"<<endl;
+
+    }
+
+    void task5(){
+        cout<<"对局结束"<<endl;
+
+    }
+
+    void operator[](int i)
+    {     
+        cout<<i<<"---";   
+        switch(i){
+            case 1: task1(); break;
+            case 2: task2(); break;
+            case 3: task3(); break;
+            case 4: task4(); break;
+            case 5: task5(); break;
+            default: cout<<"no task" << endl; break;
+            
+        }
+    }
+    int _tasknum;
+};
